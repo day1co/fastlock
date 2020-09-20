368253873f62fc4fff2a47e1e5657c878f26d356
skshim <nodelay@gmail.com>
2020-09-20 11:13:18 +0900

    Add redlock config

--- src/fast-lock.ts
+++ src/fast-lock.ts
@@ -9,6 +9,7 @@ const debug = Debug('fastlock');
 type FastLockOpts = {
   redis?: ClientOpts;
   createRedisClient?: (ClientOpts) => RedisClient;
+  redlock?: any;
 };
 
 export class FastLock {
@@ -28,23 +29,29 @@ export class FastLock {
     const createRedisClient = opts.createRedisClient || createClient;
     this.client = createRedisClient(opts.redis);
     debug(`connect redis: ${opts.redis.host}:${opts.redis.port}/${opts.redis.db}`);
-    this.redlock = new Redlock([this.client], {
-      // the expected clock drift; for more details
-      // see http://redis.io/topics/distlock
-      driftFactor: 0.01, // time in ms
 
-      // the max number of times Redlock will attempt
-      // to lock a resource before erroring
-      retryCount: 10,
+    this.redlock = new Redlock(
+      [this.client],
+      opts.redlock
+        ? opts.redlock
+        : {
+            // the expected clock drift; for more details
+            // see http://redis.io/topics/distlock
+            driftFactor: 0.01, // time in ms
 
-      // the time in ms between attempts
-      retryDelay: 200, // time in ms
+            // the max number of times Redlock will attempt
+            // to lock a resource before erroring
+            retryCount: 10,
 
-      // the max time in ms randomly added to retries
-      // to improve performance under high contention
-      // see https://www.awsarchitectureblog.com/2015/03/backoff.html
-      retryJitter: 200, // time in ms
-    });
+            // the time in ms between attempts
+            retryDelay: 200, // time in ms
+
+            // the max time in ms randomly added to retries
+            // to improve performance under high contention
+            // see https://www.awsarchitectureblog.com/2015/03/backoff.html
+            retryJitter: 200, // time in ms
+          }
+    );
     this.redlock.on('clientError', function (err: Error) {
       debug('A redis error has occured:', err);
     });
